06d913f2bd7bb53a044b3d215644ce275d095374
Frank Cooper <sbyt32@gmail.com>
2022-10-24 16:34:31 -0700

    update gitignore and cleaning comments

---
+++ data/notes.md
@@ -0,0 +1,5 @@
+# Organization
+
+cards_to_query.ndjson - sort cards
+data/tracking/set/id_name.csv - all the tracked cards, organized by set, then by id_set
+
--- scripts/query_price.py
+++ scripts/query_price.py
@@ -16,13 +16,3 @@ def query_price():
                 os.makedirs(f'data/tracking/{card_set}')
             r = scripts.util_send_response(cards['uri'])
             scripts.query_add_data(r, file_path)
-    # for cards in cards_to_query:
-    #     card_name = cards['name']
-    #     card_set = cards['set']
-    #     card_id = cards['id']
-    #     file_path = f'trackings/{card_set}/{card_id}_{scripts.util.sanitize_string(card_name)}'
-    #     if not os.path.exists(f'tracking/{card_set}'):
-    #         # ? Make a log here for making the folder
-    #         os.makedirs(f'tracking/{card_set}')
-    #     r = scripts.util.send_response(cards['uri'])
-    #     scripts.query_price.append_cards(r, file_path)
