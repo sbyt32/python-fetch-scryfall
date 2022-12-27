0cb40aa17b5195ab3943357f22a0c9054ed2e620
Frank Cooper <sbyt32@gmail.com>
2022-12-26 20:54:25 -0800

    merges data if everything is exactly the same

--- api_files/routers/internal/inventory_scripts/add_inventory.py
+++ api_files/routers/internal/inventory_scripts/add_inventory.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from api_files.request_models.add_inventory_model import AddInventory
 from psycopg.rows import dict_row
+import psycopg
 import scripts.connect.to_database as to_db
 import scripts.connect.to_requests_wrapper as to_requests_wrapper
 import logging
@@ -19,19 +20,59 @@ async def add_card_groups_with_set_id(inventory: AddInventory):
         
     if inventory.tcg_id:
         conn, cur = to_db.connect_db(row_factory = dict_row)
+
         cur.execute("""
-        INSERT INTO inventory (tcg_id, qty, buy_price, card_condition, card_variant) 
-        
-        VALUES (%s,%s,%s,%s,%s)
-        """, (
-            inventory.tcg_id,
-            inventory.qty,
-            inventory.buy_price,
-            inventory.condition,
-            inventory.card_variant
-            )
+            SELECT 
+                EXISTS (
+                    SELECT 1 
+                    FROM inventory
+                    WHERE tcg_id        = %s 
+                    AND card_condition  = %s 
+                    AND card_variant    = %s 
+                    AND buy_price       = %s
+                )
+            """, (      
+                    inventory.tcg_id,
+                    inventory.condition,
+                    inventory.card_variant,
+                    inventory.buy_price
+                )
         )
-        conn.commit()
+
+        if cur.fetchone()['exists']:
+            cur.execute("""
+                UPDATE inventory
+                SET qty = inventory.qty + %s
+                WHERE tcg_id = %s
+                AND card_condition = %s
+                AND card_variant = %s
+                AND buy_price = %s
+                """, (
+                    inventory.qty,
+                    inventory.tcg_id,
+                    inventory.condition,
+                    inventory.card_variant,
+                    inventory.buy_price
+                    )
+            )
+        else:
+            cur.execute("""
+                INSERT INTO inventory
+                VALUES (
+                    %s,
+                    %s,
+                    %s,
+                    %s,
+                    %s
+                )
+                """, (
+                    inventory.tcg_id,
+                    inventory.qty,
+                    inventory.buy_price,
+                    inventory.condition,
+                    inventory.card_variant
+                    )
+            )
 
         cur.execute("""
             SELECT * FROM inventory 
@@ -39,11 +80,14 @@ async def add_card_groups_with_set_id(inventory: AddInventory):
                 tcg_id = %s 
                 AND card_condition = %s 
                 AND card_variant = %s
+                AND buy_price = %s
             """, (
                 inventory.tcg_id, 
                 inventory.condition, 
-                inventory.card_variant
+                inventory.card_variant,
+                inventory.buy_price
                 )
         )
-        hello = cur.fetchone()
-        return hello
+        inventory_check = cur.fetchone()
+        conn.commit()
+        return inventory_check
